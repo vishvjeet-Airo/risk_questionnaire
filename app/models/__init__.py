015e73af1ae8eb5b75ac67bdfe0e7ff1edd40c88
vishvjeet thakur <vishvjeet.thakur@airodigitallabs.com>
2025-10-28 10:47:35 +0530

    added init.py in models to export all the models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,18 @@
-from .user import User, UserRole
+from .user import User, Role, BlacklistedToken, PasswordResetToken
+from .questionnaire_file import QuestionnaireFile
+from .question import Question
+from .feedback import Feedback
+from .technology import Technology
+from .sector import Sector
 
-__all__ = ["User", "UserRole"]
+__all__ = [
+    "User",
+    "Role",
+    "BlacklistedToken",
+    "PasswordResetToken",
+    "QuestionnaireFile",
+    "Question",
+    "Feedback",
+    "Technology",
+    "Sector",
+]

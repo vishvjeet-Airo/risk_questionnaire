d0620cf0c4a30f72a1ea16d1294a890ba29e279c
Yash Soni <yash.soni@samta.ai>
2025-11-06 15:32:54 +0530

    Refactor the models of Sector and Technology.

--- app/models/sector.py
+++ app/models/sector.py
@@ -6,8 +6,8 @@ from sqlalchemy import Column, DateTime, func
 class Sector(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(unique=True, max_length=255)
-    created_at: datetime | None = Field(default_factory=datetime.utcnow,sa_column=Column(DateTime(timezone=True), server_default=func.now()))
-    updated_at: datetime | None = Field(default_factory=datetime.utcnow,sa_column=Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now()))
+    created_at: Optional[datetime] = Field(default_factory=datetime.utcnow,sa_column=Column(DateTime(timezone=True), server_default=func.now()))
+    updated_at: Optional[datetime] = Field(default_factory=datetime.utcnow,sa_column=Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now()))
     deleted_at: Optional[datetime] = Field(default=None)
 
     class Config:
--- app/models/technology.py
+++ app/models/technology.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 from sqlalchemy import Column, DateTime, func
 from sqlmodel import SQLModel, Field
+from typing import Optional
 
 class Technology(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(unique=True, max_length=255)
-    created_at: datetime | None = Field(sa_column=Column(DateTime(timezone=True), server_default=func.now()))
-    updated_at: datetime | None = Field(sa_column=Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now()))
-    deleted_at: datetime | None = None
+    created_at: Optional[datetime] = Field(sa_column=Column(DateTime(timezone=True), server_default=func.now()))
+    updated_at: Optional[datetime] = Field(sa_column=Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now()))
+    deleted_at: Optional[datetime] = None
 
